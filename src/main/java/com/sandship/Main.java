8c44459f14fecaa094d6c766ad58a3989fce9ddb
Henze1 <podratchyanhenzel@gmail.com>
2024-04-19 14:10:55 +0400

    MaterialType DTO implementation

--- src/main/java/com/sandship/Main.java
+++ src/main/java/com/sandship/Main.java
@@ -1,7 +1,14 @@
 package com.sandship;
 
+import com.sandship.controllers.MaterialsController;
+import com.sandship.controllers.WarehouseController;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MaterialsController materialsController = new MaterialsController();
+
+        WarehouseController warehouseController = new WarehouseController();
+
+
     }
 }
